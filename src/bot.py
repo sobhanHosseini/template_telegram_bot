aa0c1cc2dc4dcea92be128e9f385ecbed4dcd97c
SobhanHosseini <sobhan@Sobhan>
2023-02-23 12:20:07 +0330

    w

--- src/bot.py
+++ src/bot.py
@@ -17,8 +17,8 @@ class Bot:
         self.bot.infinity_polling()
     
     def echo_all(self, message):
-        write_json(message.json, 'message.json')
-        self.bot.reply_to(message, message.text)
+        # write_json(message.json, 'message.json')
+		self.bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
 	logger.info('Bot Started')
