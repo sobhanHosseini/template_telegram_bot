7e73a87af712d0eaa2285483ab1011e49f3757d1
SobhanHosseini <sobhan@Sobhan>
2023-02-23 12:28:19 +0330

    w

--- src/bot.py
+++ src/bot.py
@@ -2,9 +2,14 @@ import os
 
 import telebot
 from loguru import logger
+from telebot import types
 
 from src.utils.io import write_json
 
+markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard= True)
+itembtn1 = types.KeyboardButton('Connect')
+itembtn2 = types.KeyboardButton('Settings')
+markup.add(itembtn1, itembtn2)
 
 class Bot:
     
@@ -17,7 +22,10 @@ class Bot:
         self.bot.infinity_polling()
     
     def echo_all(self, message):
-        self.bot.send_message(message.chat.id, message.text)
+        self.bot.send_message(
+            message.chat.id, message.text,
+            reply_markup=markup
+            )
 		
 if __name__ == '__main__':
 	logger.info('Bot Started')
