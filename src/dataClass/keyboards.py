e8b9490200c4352ea29786963d8a71a7c44ddb1a
SobhanHosseini <sobhan@Sobhan>
2023-02-23 13:19:45 +0330

    s

--- src/dataClass/keyboards.py
+++ src/dataClass/keyboards.py
@@ -11,13 +11,13 @@ keys = keys.Keys()
 class Keyboards:
     main:List = create_keyboard(keys.random_connect, keys.setting)
     
-    def create_keyboard(*keys, row_width=2, resize_keyboard=True):
-        markup =  types.ReplyKeyboardMarkup(
-            row_width=row_width,
-            resize_keyboard=resize_keyboard
-        )
-        
-        buttons = map(types.KeyboardButton, keys)
-        markup.add(*buttons)
-        
-        return markup
+def create_keyboard(*keys, row_width=2, resize_keyboard=True):
+    markup =  types.ReplyKeyboardMarkup(
+        row_width=row_width,
+        resize_keyboard=resize_keyboard
+    )
+    
+    buttons = map(types.KeyboardButton, keys)
+    markup.add(*buttons)
+    
+    return markup
