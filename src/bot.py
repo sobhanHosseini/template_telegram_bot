5fa5e5e2dc910864ceafa1346d3fb4970c77cda9
SobhanHosseini <sobhan@Sobhan>
2023-02-23 12:07:05 +0330

    w

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,26 @@
-import telebot
 import os
+
+import telebot
 from loguru import logger
+
+from src.utils.io import write_json
+
 
 class Bot:
     
     def __init__(self) -> None:
         self.bot = telebot.TeleBot(os.environ['ANONYMOUS_BOT_TOKEN'])
+        self.echo_all = self.bot.message_handler(func=lambda message: True)(self.echo_all)
     
     def run(self):
         logger.info('Bot is running...')
         self.bot.infinity_polling()
-        
+    
+    def echo_all(self, message):
+        write_json(message.json, 'message.json')
+        self.bot.reply_to(message, message.text)
 
-if __name__=='__main__':
+if __name__ == '__main__':
 	logger.info('Bot Started')
 	bot = Bot()
 	bot.run()
